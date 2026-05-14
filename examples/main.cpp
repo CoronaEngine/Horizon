47d40467b07b6481919671cc45ad086cf0b98096
Michael <1216866818@qq.com>
2026-05-14 15:12:37 +0800

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,12 +1,20 @@
-#include "default/default_test.hpp"
-//#include "example_2.hpp"
-//#include "example_3.hpp"
+#include "example_env/example_env.hpp"
+#include "example_default/default_test.hpp"
+#include "example_baseline/example_baseline.hpp"
 
 int main()
 {
-    default_test();
-    //example_2();
-    //example_3();
+    try
+    {
+        //run_example_default();
+        run_example_env();
+        //run_example_baseline();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        
+    }
 
     return 0;
 }
