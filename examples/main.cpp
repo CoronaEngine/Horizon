3d31ade7276b2a81aba5bbd922ed7a8e04c45708
Michael <1216866818@qq.com>
2026-05-15 18:26:45 +0800

    fix: 成功运行 run_example_env()

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,19 +1,24 @@
-#include "example_env/example_env.hpp"
-#include "example_default/default_test.hpp"
-#include "example_baseline/example_baseline.hpp"
+#include "example_env/example_env.h"
+//#include "example_default/example_default.hpp"
+//#include "example_baseline/example_baseline.hpp"
+
+#include <exception>
+#include <iostream>
 
 int main()
 {
     try
     {
-        //run_example_default();
         run_example_env();
+        //run_example_default();
         //run_example_baseline();
+        //run_example_baseline_tutorial();
+        //run_example_baseline_glsl();
+        //run_example_baseline_edsl();
     }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
-        
     }
 
     return 0;
