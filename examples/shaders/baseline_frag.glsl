119af7ebd48d1898a648719d89823e67334fae98
Michael <1216866818@qq.com>
2026-05-22 10:12:02 +0800

    fix: 使用 VulkanTutorial 原生 shader

--- examples/shaders/baseline_frag.glsl
+++ examples/shaders/baseline_frag.glsl
@@ -7,7 +7,6 @@ layout(location = 1) in vec2 fragTexCoord;
 
 layout(location = 0) out vec4 outColor;
 
-void main()
-{
-    outColor = texture(texSampler, fragTexCoord) * vec4(fragColor, 1.0);
+void main() {
+    outColor = texture(texSampler, fragTexCoord);
 }
