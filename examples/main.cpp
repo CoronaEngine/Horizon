7f923fabd9108a759c72b01acd3ba392d36cbcad
Michael <1216866818@qq.com>
2026-05-22 11:38:49 +0800

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,6 @@
 
-#include "example_default/example_default.h"
 #include "example_baseline/example_baseline.h"
+#include "example_default/example_default.h"
 #include "example_glsl/example_glsl.h"
 #include "example_edsl/example_edsl.h"
 
@@ -11,6 +11,8 @@ int main()
 {
     try
     {
+        // 每一个方法都代表一个独立的示例，展示了不同的功能或者后端实现。可以根据需要选择运行其中一个或者多个示例。
+
         run_example_default();
         //run_example_baseline();
         //run_example_glsl();
