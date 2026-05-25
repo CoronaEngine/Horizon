7ec6a0107f57a83a5816fcf1b7da1a0b7a769ff4
Michael <1216866818@qq.com>
2026-05-25 19:19:55 +0800

    Update device_manager.h

---
+++ src/hardware_wrapper_vulkan/hardware/device_manager.h
@@ -0,0 +1,17 @@
+
+
+namespace Corona::Horizon
+{
+    class DeviceManager
+    {
+    public:
+        DeviceManager();
+        ~DeviceManager();
+
+        //void initialize(const CreateCallback& createCallback, const VkInstance& vkInstance, const VkPhysicalDevice& physicalDevice);
+        //void shutdown() noexcept;
+
+    private:
+    };
+
+}
