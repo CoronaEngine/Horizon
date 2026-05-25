ab815176cffd14f96f0741c81f9a09b9e85aa83c
Michael <1216866818@qq.com>
2026-05-25 19:24:32 +0800

    Update resource_pool.h

--- src/hardware_wrapper_vulkan/resource_pool.h
+++ src/hardware_wrapper_vulkan/resource_pool.h
@@ -1,87 +1,60 @@
 #pragma once
 
-#include <cstdint>
-#include <memory>
-
-#include "hardware_wrapper_vulkan/HardwareVulkan/ResourceManager.h"
 #include "corona/kernel/utils/storage.h"
 
-class DisplayManager;
-struct ComputePipelineVulkan;
-struct HardwareExecutorVulkan;
-struct RasterizerPipelineVulkan;
-
-struct BufferWrap
+namespace Corona::Horizon
 {
-    HardwareBufferDesc desc;
+    class DisplayManager;
+    class ComputePipelineVulkan;
+    class HardwareExecutorVulkan;
+    class RasterizerPipelineVulkan;
 
-    uint64_t allocation_size{0}; // 实际分配大小。外部导入或者对齐后，可能大于 desc.byte_size。
-    uint64_t ref_count{1};
+    struct BufferWrap
+    {
+        HardwareBufferDesc desc;
 
-    VkBuffer buffer_handle{VK_NULL_HANDLE};
-    VkBufferUsageFlags buffer_usage{0};
-    VmaAllocation buffer_alloc{VK_NULL_HANDLE};
-    VmaAllocationInfo buffer_alloc_info{};
+        uint64_t allocation_size{0}; // 实际分配大小。外部导入或者对齐后，可能大于 desc.byte_size。
+        uint64_t ref_count{1};
 
-    bool host_imported_manual_bind{false};
-    bool imported{false};
+        VkBuffer buffer_handle{VK_NULL_HANDLE};
+        VkBufferUsageFlags buffer_usage{0};
+        VmaAllocation buffer_alloc{VK_NULL_HANDLE};
+        VmaAllocationInfo buffer_alloc_info{};
 
-    int32_t bindless_index{-1};
+        bool host_imported_manual_bind{false};
+        bool imported{false};
 
-    DeviceManager *device_manager{nullptr};
-    ResourceManager *resource_manager{nullptr};
-};
+        int32_t bindless_index{-1};
 
-using BufferHardwareStorage = Corona::Kernel::Utils::Storage<ResourceManager::BufferHardwareWrap>;
-using ImageHardwareStorage = Corona::Kernel::Utils::Storage<ResourceManager::ImageHardwareWrap>;
+        DeviceManager* device_manager{nullptr};
+        ResourceManager* resource_manager{nullptr};
+    };
 
-struct RasterizerPipelineWrap
-{
-    RasterizerPipelineVulkan *impl{nullptr};
-    uint64_t refCount{1};
-};
+    extern Corona::Kernel::Utils::Storage<ResourceManager::BufferWrap> g_buffer_storages;
 
-using RasterizerPipelineStorage = Corona::Kernel::Utils::Storage<RasterizerPipelineWrap>;
+    struct RasterizerPipelineWrap
+    {
+        RasterizerPipelineVulkan *impl{nullptr};
+        uint64_t refCount{1};
+    };
 
-struct ComputePipelineWrap
-{
-    ComputePipelineVulkan *impl{nullptr};
-    uint64_t refCount{1};
-};
+    struct ComputePipelineWrap
+    {
+        ComputePipelineVulkan *impl{nullptr};
+        uint64_t refCount{1};
+    };
 
-using ComputePipelineStorage = Corona::Kernel::Utils::Storage<ComputePipelineWrap>;
+    struct DisplayerHardwareWrap
+    {
+        void *displaySurface{nullptr};
+        std::shared_ptr<DisplayManager> displayManager;
+        uint64_t refCount{1};
+    };
 
-struct DisplayerHardwareWrap
-{
-    void *displaySurface{nullptr};
-    std::shared_ptr<DisplayManager> displayManager;
-    uint64_t refCount{1};
-};
+    struct ExecutorWrap
+    {
+        HardwareExecutorVulkan *impl{nullptr};
+        uint64_t refCount{1};
+    };
 
-using DisplayerHardwareStorage = Corona::Kernel::Utils::Storage<DisplayerHardwareWrap>;
-
-struct ExecutorWrap
-{
-    HardwareExecutorVulkan *impl{nullptr};
-    uint64_t refCount{1};
-};
-
-using ExecutorStorage = Corona::Kernel::Utils::Storage<ExecutorWrap>;
-
-struct PushConstantWrap
-{
-    uint8_t *data{nullptr};
-    uint64_t size{0};
-    uint64_t refCount{1};
-    bool isSub{false};
-};
-
-using PushConstantStorage = Corona::Kernel::Utils::Storage<PushConstantWrap>;
-
-extern BufferHardwareStorage globalBufferStorages;
-extern ImageHardwareStorage globalImageStorages;
-extern RasterizerPipelineStorage gRasterizerPipelineStorage;
-extern ComputePipelineStorage gComputePipelineStorage;
-extern DisplayerHardwareStorage globalDisplayerStorages;
-extern ExecutorStorage gExecutorStorage;
-extern PushConstantStorage globalPushConstantStorages;
+}
