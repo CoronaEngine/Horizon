701f312a9c23c4d229b876eee885dede6fa76b90
Michael <1216866818@qq.com>
2026-05-26 18:02:49 +0800

    fix: 暂时在 example 里测试

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,8 +1,9 @@
 
-#include "example_baseline/example_baseline.h"
-#include "example_default/example_default.h"
-#include "example_glsl/example_glsl.h"
-#include "example_edsl/example_edsl.h"
+//#include "example_baseline/example_baseline.h"
+//#include "example_default/example_default.h"
+//#include "example_glsl/example_glsl.h"
+//#include "example_edsl/example_edsl.h"
+#include "hardware_wrapper_vulkan/hardware_context.h"
 
 #include <exception>
 #include <iostream>
@@ -13,10 +14,12 @@ int main()
     {
         // 每一个方法都代表一个独立的示例，展示了不同的功能或者后端实现。可以根据需要选择运行其中一个或者多个示例。
 
-        run_example_default();
+        //run_example_default();
         //run_example_baseline();
         //run_example_glsl();
         //run_example_edsl();
+        Corona::Horizon::HardwareContext context;
+
     }
     catch (const std::exception &e)
     {
