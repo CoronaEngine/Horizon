ce3a44122596123dddd43260d2f6d66e557935a5
Michael <1216866818@qq.com>
2026-05-26 18:03:31 +0800

    fix: 先注释掉没用的代码

--- src/hardware_wrapper_vulkan/resource_pool.cpp
+++ src/hardware_wrapper_vulkan/resource_pool.cpp
@@ -1,8 +1,8 @@
-#include "resource_pool.h"
-
-Corona::Kernel::Utils::Storage<BufferWrap> g_buffer_storages;
-Corona::Kernel::Utils::Storage<ImageWrap> g_image_storages;
-Corona::Kernel::Utils::Storage<RasterizerPipelineWrap> g_rasterizer_pipeline_storages;
-Corona::Kernel::Utils::Storage<ComputePipelineWrap> g_compute_pipeline_storages;
-Corona::Kernel::Utils::Storage<ExecutorWrap> g_executor_storages;
-Corona::Kernel::Utils::Storage<DisplayerWrap> g_displayer_storages;
+//#include "resource_pool.h"
+//
+//Corona::Kernel::Utils::Storage<BufferWrap> g_buffer_storages;
+//Corona::Kernel::Utils::Storage<ImageWrap> g_image_storages;
+//Corona::Kernel::Utils::Storage<RasterizerPipelineWrap> g_rasterizer_pipeline_storages;
+//Corona::Kernel::Utils::Storage<ComputePipelineWrap> g_compute_pipeline_storages;
+//Corona::Kernel::Utils::Storage<ExecutorWrap> g_executor_storages;
+//Corona::Kernel::Utils::Storage<DisplayerWrap> g_displayer_storages;
--- src/hardware_wrapper_vulkan/resource_pool.h
+++ src/hardware_wrapper_vulkan/resource_pool.h
@@ -1,60 +1,60 @@
-#pragma once
-
-#include "corona/kernel/utils/storage.h"
-
-namespace Corona::Horizon
-{
-    class DisplayManager;
-    class ComputePipelineVulkan;
-    class HardwareExecutorVulkan;
-    class RasterizerPipelineVulkan;
-
-    struct BufferWrap
-    {
-        HardwareBufferDesc desc;
-
-        uint64_t allocation_size{0}; // 实际分配大小。外部导入或者对齐后，可能大于 desc.byte_size。
-        uint64_t ref_count{1};
-
-        VkBuffer buffer_handle{VK_NULL_HANDLE};
-        VkBufferUsageFlags buffer_usage{0};
-        VmaAllocation buffer_alloc{VK_NULL_HANDLE};
-        VmaAllocationInfo buffer_alloc_info{};
-
-        bool host_imported_manual_bind{false};
-        bool imported{false};
-
-        int32_t bindless_index{-1};
-
-        DeviceManager* device_manager{nullptr};
-        ResourceManager* resource_manager{nullptr};
-    };
-
-    extern Corona::Kernel::Utils::Storage<ResourceManager::BufferWrap> g_buffer_storages;
-
-    struct RasterizerPipelineWrap
-    {
-        RasterizerPipelineVulkan *impl{nullptr};
-        uint64_t refCount{1};
-    };
-
-    struct ComputePipelineWrap
-    {
-        ComputePipelineVulkan *impl{nullptr};
-        uint64_t refCount{1};
-    };
-
-    struct DisplayerHardwareWrap
-    {
-        void *displaySurface{nullptr};
-        std::shared_ptr<DisplayManager> displayManager;
-        uint64_t refCount{1};
-    };
-
-    struct ExecutorWrap
-    {
-        HardwareExecutorVulkan *impl{nullptr};
-        uint64_t refCount{1};
-    };
-
-}
+//#pragma once
+//
+//#include "corona/kernel/utils/storage.h"
+//
+//namespace Corona::Horizon
+//{
+//    class DisplayManager;
+//    class ComputePipelineVulkan;
+//    class HardwareExecutorVulkan;
+//    class RasterizerPipelineVulkan;
+//
+//    struct BufferWrap
+//    {
+//        HardwareBufferDesc desc;
+//
+//        uint64_t allocation_size{0}; // 实际分配大小。外部导入或者对齐后，可能大于 desc.byte_size。
+//        uint64_t ref_count{1};
+//
+//        VkBuffer buffer_handle{VK_NULL_HANDLE};
+//        VkBufferUsageFlags buffer_usage{0};
+//        VmaAllocation buffer_alloc{VK_NULL_HANDLE};
+//        VmaAllocationInfo buffer_alloc_info{};
+//
+//        bool host_imported_manual_bind{false};
+//        bool imported{false};
+//
+//        int32_t bindless_index{-1};
+//
+//        DeviceManager* device_manager{nullptr};
+//        ResourceManager* resource_manager{nullptr};
+//    };
+//
+//    extern Corona::Kernel::Utils::Storage<ResourceManager::BufferWrap> g_buffer_storages;
+//
+//    struct RasterizerPipelineWrap
+//    {
+//        RasterizerPipelineVulkan *impl{nullptr};
+//        uint64_t refCount{1};
+//    };
+//
+//    struct ComputePipelineWrap
+//    {
+//        ComputePipelineVulkan *impl{nullptr};
+//        uint64_t refCount{1};
+//    };
+//
+//    struct DisplayerHardwareWrap
+//    {
+//        void *displaySurface{nullptr};
+//        std::shared_ptr<DisplayManager> displayManager;
+//        uint64_t refCount{1};
+//    };
+//
+//    struct ExecutorWrap
+//    {
+//        HardwareExecutorVulkan *impl{nullptr};
+//        uint64_t refCount{1};
+//    };
+//
+//}
