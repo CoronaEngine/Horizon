07572b2678c2a5cc8574d10f3ac80fe318be68fc
Michael <1216866818@qq.com>
2026-06-13 14:21:40 +0800

    fix：全部替换为原来的 github

--- cmake/HorizonCoreDependencies.cmake
+++ cmake/HorizonCoreDependencies.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 # ======================== Helicon 依赖 ========================
 horizon_fetchcontent_declare(
     pfr
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/pfr.git
+    GIT_REPOSITORY https://github.com/boostorg/pfr.git
     GIT_TAG develop
     EXCLUDE_FROM_ALL
 )
@@ -11,7 +11,7 @@ FetchContent_MakeAvailable(pfr)
 
 horizon_fetchcontent_declare(
     ktm
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/ktm.git
+    GIT_REPOSITORY https://github.com/YGXXD/ktm.git
     GIT_TAG main
     EXCLUDE_FROM_ALL
     SOURCE_SUBDIR cmake/horizon-skip-subdir
@@ -20,7 +20,7 @@ FetchContent_MakeAvailable(ktm)
 
 horizon_fetchcontent_declare(
     preprocessor
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/preprocessor.git
+    GIT_REPOSITORY https://github.com/boostorg/preprocessor.git
     GIT_TAG develop
     EXCLUDE_FROM_ALL
 )
@@ -28,7 +28,7 @@ FetchContent_MakeAvailable(preprocessor)
 
 horizon_fetchcontent_declare(
     glslang
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/glslang.git
+    GIT_REPOSITORY https://github.com/KhronosGroup/glslang.git
     GIT_TAG main
     EXCLUDE_FROM_ALL
 )
@@ -39,7 +39,7 @@ FetchContent_MakeAvailable(glslang)
 
 horizon_fetchcontent_declare(
     SPIRV-Cross
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/SPIRV-Cross.git
+    GIT_REPOSITORY https://github.com/KhronosGroup/SPIRV-Cross.git
     GIT_TAG main
     EXCLUDE_FROM_ALL
 )
@@ -56,7 +56,7 @@ FetchContent_MakeAvailable(SPIRV-Cross)
 
 horizon_fetchcontent_declare(
     SPIRV-Headers
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/SPIRV-Headers.git
+    GIT_REPOSITORY https://github.com/KhronosGroup/SPIRV-Headers.git
     GIT_TAG vulkan-sdk-1.4.341
     EXCLUDE_FROM_ALL
 )
@@ -64,7 +64,7 @@ FetchContent_MakeAvailable(SPIRV-Headers)
 
 horizon_fetchcontent_declare(
     SPIRV-Tools
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/SPIRV-Tools.git
+    GIT_REPOSITORY https://github.com/KhronosGroup/SPIRV-Tools.git
     GIT_TAG vulkan-sdk-1.4.341
     EXCLUDE_FROM_ALL
 )
@@ -96,7 +96,7 @@ if(HORIZON_BUILD_OCARINA AND DEFINED ENV{CUDA_PATH})
 set(FMT_OS OFF CACHE BOOL "" FORCE)
 horizon_fetchcontent_declare(
     fmt
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/fmt.git
+    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
     GIT_TAG main
     EXCLUDE_FROM_ALL
 )
@@ -111,7 +111,7 @@ target_include_directories(fmt-header-only INTERFACE ${fmt_SOURCE_DIR}/include)
 set(SPDLOG_FMT_EXTERNAL ON CACHE BOOL "" FORCE)
 horizon_fetchcontent_declare(
     spdlog
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/spdlog.git
+    GIT_REPOSITORY https://github.com/gabime/spdlog.git
     GIT_TAG v1.x
     EXCLUDE_FROM_ALL
 )
@@ -123,7 +123,7 @@ target_link_libraries(spdlog PUBLIC fmt::fmt-header-only)
 # ---- xxHash (header-only, 不使用其顶层 CMakeLists) ----
 horizon_fetchcontent_declare(
     xxhash
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/xxHash.git
+    GIT_REPOSITORY https://github.com/Cyan4973/xxHash.git
     GIT_TAG dev
     EXCLUDE_FROM_ALL
 )
@@ -142,7 +142,7 @@ set(VOLK_PULL_IN_VULKAN OFF) # We will provide Vulkan-Headers ourselves
 set(VOLK_INSTALL ${HORIZON_ENABLE_DEPENDENCY_INSTALL} CACHE BOOL "" FORCE)
 horizon_fetchcontent_declare(
     volk
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/volk.git
+    GIT_REPOSITORY https://github.com/zeux/volk.git
     GIT_TAG master
     EXCLUDE_FROM_ALL
 )
@@ -150,7 +150,7 @@ FetchContent_MakeAvailable(volk)
 
 horizon_fetchcontent_declare(
     Vulkan-Headers
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/Vulkan-Headers.git
+    GIT_REPOSITORY https://github.com/KhronosGroup/Vulkan-Headers.git
     GIT_TAG v1.4.341
     EXCLUDE_FROM_ALL
 )
@@ -159,7 +159,7 @@ FetchContent_MakeAvailable(Vulkan-Headers)
 
 horizon_fetchcontent_declare(
     VulkanMemoryAllocator
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/VulkanMemoryAllocator.git
+    GIT_REPOSITORY https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator.git
     GIT_TAG master
     EXCLUDE_FROM_ALL
 )
--- cmake/HorizonExampleDependencies.cmake
+++ cmake/HorizonExampleDependencies.cmake
@@ -2,7 +2,7 @@ include_guard(GLOBAL)
 
 horizon_fetchcontent_declare(
     stb
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/stb.git
+    GIT_REPOSITORY https://github.com/nothings/stb.git
     GIT_TAG master
     EXCLUDE_FROM_ALL
 )
@@ -10,7 +10,7 @@ FetchContent_MakeAvailable(stb)
 
 horizon_fetchcontent_declare(
     glfw
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/glfw.git
+    GIT_REPOSITORY https://github.com/glfw/glfw.git
     GIT_TAG master
     EXCLUDE_FROM_ALL
 )
@@ -18,7 +18,7 @@ FetchContent_MakeAvailable(glfw)
 
 horizon_fetchcontent_declare(
     tinyobjloader
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/tinyobjloader.git
+    GIT_REPOSITORY https://github.com/tinyobjloader/tinyobjloader.git
     GIT_TAG release
     EXCLUDE_FROM_ALL
 )
@@ -26,7 +26,7 @@ FetchContent_MakeAvailable(tinyobjloader)
 
 horizon_fetchcontent_declare(
     glm
-    GIT_REPOSITORY https://ckzgit.iepose.cn/michael/glm.git
+    GIT_REPOSITORY https://github.com/g-truc/glm.git
     GIT_TAG master
     EXCLUDE_FROM_ALL
 )
