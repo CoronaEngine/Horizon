96f1d719e6f55d1be3d50a9d2efda55af541b24d
GraphZou <906520989@qq.com>
2026-07-30 04:17:05 +0800

    默认sponza

--- examples/main.cpp
+++ examples/main.cpp
@@ -20,7 +20,7 @@
 
 int main(int argc, char **argv)
 {
-    const std::string_view mode = argc > 1 ? std::string_view(argv[1]) : std::string_view("assao");
+    const std::string_view mode = argc > 1 ? std::string_view(argv[1]) : std::string_view("sponza");
 
     if (mode == "baseline")
     {
