50344b9bcae2c4fca3add7a85b3b91bdcca3b7a9
GraphZou <906520989@qq.com>
2026-08-13 13:37:41 +0800

    清理

--- include/format.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Compatibility shim: format.h has been merged into horizon.h.
-// Kept only for internal source files that still #include "format.h".
-#include "horizon.h"
--- include/resource.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Compatibility shim: resource.h has been merged into horizon.h.
-// Kept only for internal source files that still #include "resource.h".
-#include "horizon.h"
