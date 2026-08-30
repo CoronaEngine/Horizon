20cf31d2e94c3c1379f0e730832ef1a61bb5be9e
GraphZou <906520989@qq.com>
2026-08-30 21:23:01 +0800

    清理文件

--- build_examples.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Insiders\VC\Auxiliary\Build\vcvars64.bat" >nul
-cd /d e:\Horizon\build\conan\examples\release
-cmake --build . --config Release --target HorizonExamples
